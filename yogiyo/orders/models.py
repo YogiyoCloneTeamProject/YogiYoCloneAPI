a4a6223d1ec43a773c6b0baba23086f30ac44b76
withHappy <hap2.dev@gmail.com>
2020-08-26 23:43:39 +0900

    add order_option_group , order_option, order_menu model

--- yogiyo/orders/models.py
+++ yogiyo/orders/models.py
@@ -6,3 +6,20 @@ class Order(models.Model):
     restaurant = models.ForeignKey('restaurants.Restaurant', on_delete=models.CASCADE)
     order_time = models.DateTimeField(auto_now_add=True)
     # status = models.TextChoices() todo order status choice field
+
+
+class OrderMenu(models.Model):
+    menu = models.ForeignKey('restaurants.Menu', on_delete=models.CASCADE)
+    order = models.ForeignKey('Order', on_delete=models.CASCADE)
+    count = models.PositiveIntegerField()
+
+
+class OrderOptionGroup(models.Model):
+    order_menu = models.ForeignKey('OrderMenu', on_delete=models.CASCADE)
+    name = models.CharField(max_length=40)
+
+
+class OrderOption(models.Model):
+    order_option_group = models.ForeignKey('OrderOptionGroup', on_delete=models.CASCADE)
+    name = models.CharField(max_length=40)
+    price = models.PositiveIntegerField()
