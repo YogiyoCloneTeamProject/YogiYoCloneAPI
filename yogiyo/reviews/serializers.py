0d040f900d953a7dc5910d2784f00c3bf3fd30af
withHappy <hap2.dev@gmail.com>
2020-09-23 20:50:32 +0900

    add review duplicate 400 error

--- yogiyo/reviews/serializers.py
+++ yogiyo/reviews/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from rest_framework.validators import UniqueTogetherValidator
 
 from reviews.models import Review
 
@@ -14,5 +16,13 @@ class ReviewListSerializer(serializers.ModelSerializer):
 class ReviewCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = ('id', 'taste', 'amount', 'delivery', 'restaurant', 'caption', 'order_menu', 'rating', 'owner', 'order')
-        read_only_fields = ('owner', 'order','rating')
+        fields = ('id', 'taste', 'amount', 'delivery', 'restaurant', 'caption', 'menu_name', 'rating', 'owner', 'order')
+        read_only_fields = ('owner', 'order', 'rating', 'menu_name', 'restaurant')
+
+    def validate(self, attrs):
+        """request order_pk 가 리뷰 모델에 있는지 검증 """
+        order_pk = self.context['view'].kwargs['order_pk']
+        is_order = Review.objects.filter(order_id=order_pk).first()
+        if is_order is not None:
+            raise ValidationError('this order already exists in Review models')
+        return attrs
--- yogiyo/reviews/views.py
+++ yogiyo/reviews/views.py
@@ -22,16 +22,32 @@ class ReviewViewSet(mixins.ListModelMixin, mixins.CreateModelMixin, mixins.Destr
     def perform_create(self, serializer):
         if 'order_pk' in self.kwargs:
             order = get_object_or_404(Order, id=self.kwargs.get('order_pk'))
-            rating = int((self.request.data['taste']+self.request.data['delivery'] + self.request.data['amount'])/3)
+            rating = int((self.request.data['taste'] + self.request.data['delivery'] + self.request.data['amount']) / 3)
+
+            # 메뉴 이름/count(옵션끄룹이름(옵션이름,옵션이름), 옵션그룹이름(옵션이름,옵션이름)), 메뉴2/count…
+            menu_list = []
+            for order_menu in order.order_menu.all():
+                menu = order_menu.name + '/' + str(order_menu.count)
+                order_option_group_list = []
+                order_option_group = ''
+                for order_option_group_obj in order_menu.order_option_group.all():
+                    order_option_group += '(' + order_option_group_obj.name
+                    for order_option_obj in order_option_group_obj.order_option.all():
+                        order_option_group += '(' + order_option_obj.name + ')'
+                    order_option_group += ')'
+                    order_option_group_list.append(order_option_group)
+                menu += ','.join(order_option_group_list)
+                menu_list.append(menu)
+            menu_name = ','.join(menu_list)
 
             serializer.save(
                 owner=self.request.user,
                 order=order,
                 rating=rating,
                 restaurant=order.restaurant,
-                order_menu=order.order_menu  # string  으로...
+                menu_name=menu_name
             )
 
     # def get_queryset(self):
-        # queryset = super().get_queryset().filter(=self.kwargs.get('order_pk'))
-        # return queryset
+    # queryset = super().get_queryset().filter(=self.kwargs.get('order_pk'))
+    # return queryset
