c8972518556d69de5af2154d3ca54d1728de9dc6
김주원 <kjw11077@naver.com>
2020-09-27 16:51:11 +0900

    update Profile to OneToOneField

--- yogiyo/users/models.py
+++ yogiyo/users/models.py
@@ -38,7 +38,7 @@ class UserManager(BaseUserManager):
 
 
 class Profile(models.Model):
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    user = models.OneToOneField('User', on_delete=models.CASCADE, related_name='profile')
     point = models.PositiveIntegerField(null=True, blank=True)
     nickname = models.CharField(max_length=20, null=True, blank=True)
     phone_num = PhoneNumberField(null=True, blank=True, unique=True)
