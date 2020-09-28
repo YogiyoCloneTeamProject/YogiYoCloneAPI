a0f7155a2ed383fe7c77cd26b50c02b69baab5be
김주원 <kjw11077@naver.com>
2020-09-28 17:35:47 +0900

    update url

--- yogiyo/yogiyo/urls.py
+++ yogiyo/yogiyo/urls.py
@@ -18,6 +18,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -30,18 +31,24 @@ urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # swagger 정보 설정, 관련 엔드포인트 추가
 # swagger 엔드포인트는 DEBUG Mode 에서만 노출
-# schema_view = get_schema_view(
-#     openapi.Info(
-#         title="Yogiyo API",
-#         default_version='v1',
-#         description="Test description",
-#         terms_of_service="https://www.google.com/policies/terms/",
-#         contact=openapi.Contact(email="hap2.dev@gmail.com"),
-#         license=openapi.License(name="BSD License"),
-#     ),
-#     public=True,
-#     permission_classes=(permissions.AllowAny,),
-# )
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Yogiyo API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="hap2.dev@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 # urlpatterns += [
 #     path(r'^redoc/$', schema_view.with_ui('redoc'))
 # ]
+urlpatterns += [
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='swagger-ui')
+]
