2ef0e2951c74516d292165f50e4f60c3c87c6014
withHappy <hap2.dev@gmail.com>
2020-09-30 22:55:14 +0900

    create review_comment -> review_comment_count + 1

--- yogiyo/reviews/models.py
+++ yogiyo/reviews/models.py
@@ -33,16 +33,16 @@ class Review(models.Model):
         """리뷰 생성할 때 해당 레스토랑의 별점을 반영한다"""
         if not settings.CRAWLING:  # 크롤링 시 사용 X
             self.restaurant.average_taste = (
-                                                        self.restaurant.average_taste * self.restaurant.review_count + self.taste) / (
-                                                        self.restaurant.review_count + 1)
+                                                    self.restaurant.average_taste * self.restaurant.review_count + self.taste) / (
+                                                    self.restaurant.review_count + 1)
             self.restaurant.average_delivery = (
-                                                           self.restaurant.average_delivery * self.restaurant.review_count + self.delivery) / (
-                                                           self.restaurant.review_count + 1)
+                                                       self.restaurant.average_delivery * self.restaurant.review_count + self.delivery) / (
+                                                       self.restaurant.review_count + 1)
             self.restaurant.average_amount = (
-                                                         self.restaurant.average_amount * self.restaurant.review_count + self.amount) / (
-                                                         self.restaurant.review_count + 1)
+                                                     self.restaurant.average_amount * self.restaurant.review_count + self.amount) / (
+                                                     self.restaurant.review_count + 1)
             self.restaurant.average_rating = (
-                                                         self.restaurant.average_taste + self.restaurant.average_delivery + self.restaurant.average_amount) / 3
+                                                     self.restaurant.average_taste + self.restaurant.average_delivery + self.restaurant.average_amount) / 3
 
         self.restaurant.review_count = F('review_count') + 1
         self.restaurant.save()
@@ -61,7 +61,6 @@ class ReviewComment(models.Model):
 
     def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
         super().save(force_insert, force_update, using, update_fields)
-        """ restaurant - review_count +1 """
-        print("g")
-        print(Restaurant.objects.get(id=self.review.restaurant_id).review_count)
-        Restaurant.objects.filter(id=self.review.restaurant_id).update(review_count=F('review_count') + 1)
+        """ restaurant - review_comment_count +1 """
+        Restaurant.objects.filter(id=self.review.restaurant_id).update(
+            review_comment_count=F('review_comment_count') + 1)
