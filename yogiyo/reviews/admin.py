4537dc24e73634a3baf3dbcb61d3e653dd181946
김주원 <kjw11077@naver.com>
2020-10-03 22:33:33 +0900

    add ReviewAdmin

--- yogiyo/reviews/admin.py
+++ yogiyo/reviews/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from reviews.models import *
 
-# Register your models here.
+
+class ReviewImageInline(admin.TabularInline):
+    model = ReviewImage
+
+
+class OwnerCommentInline(admin.TabularInline):
+    model = OwnerComment
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['id', 'owner', 'restaurant']
+    inlines = [ReviewImageInline, OwnerCommentInline]
