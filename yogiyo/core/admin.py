2cd9c0853abf3e5ba714c4256d80a1ca927c583a
김주원 <kjw11077@naver.com>
2020-10-03 21:20:39 +0900

    add restaurants admin

--- yogiyo/core/admin.py
+++ yogiyo/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+
+class CustomModelAdmin(admin.ModelAdmin):
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
