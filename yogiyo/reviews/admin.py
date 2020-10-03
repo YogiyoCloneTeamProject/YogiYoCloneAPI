06b9f7fe1204d8183de26d11ae0dcdda1594f7a6
김주원 <kjw11077@naver.com>
2020-10-04 08:55:20 +0900

    add ReviewAdmin

--- yogiyo/reviews/admin.py
+++ yogiyo/reviews/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from reviews.models import *
 
 
@@ -14,3 +16,21 @@ class OwnerCommentInline(admin.TabularInline):
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ['id', 'owner', 'restaurant']
     inlines = [ReviewImageInline, OwnerCommentInline]
+
+
+@admin.register(ReviewImage)
+class ReviewImageAdmin(admin.ModelAdmin):
+    list_display = ['id', 'image']
+    readonly_fields = ['preview_image']
+
+    def preview_image(self, obj):
+        return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
+            url=obj.image.url,
+            width=obj.image.width,
+            height=obj.image.height)
+        )
+
+
+@admin.register(OwnerComment)
+class OwnerCommentAdmin(admin.ModelAdmin):
+    list_display = ['id']
