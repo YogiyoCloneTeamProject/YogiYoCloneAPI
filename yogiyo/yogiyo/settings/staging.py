4a060f8c31fa2a6d0c78068746bfb46827e46d04
김주원 <kjw11077@naver.com>
2020-10-06 21:56:30 +0900

    update staging

--- yogiyo/yogiyo/settings/staging.py
+++ yogiyo/yogiyo/settings/staging.py
@@ -2,3 +2,14 @@ from .base import *
 
 PROFILE = 'staging'
 DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': os.environ['DEPLOY_DB_HOST'],
+        'NAME': os.environ['DEPLOY_DB_NAME'],
+        'USER': os.environ['DEPLOY_DB_USER'],
+        'PASSWORD': os.environ['DEPLOY_DB_PASSWORD'],
+        'PORT': os.environ['DEPLOY_DB_PORT'],
+    }
+}
