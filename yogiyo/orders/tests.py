91048c9430af7ad6d102542290a2b41c108e3e9b
withHappy <hap2.dev@gmail.com>
2020-10-13 23:58:05 +0900

    add order test menu count

--- yogiyo/orders/tests.py
+++ yogiyo/orders/tests.py
@@ -583,6 +583,8 @@ class OrderCreateTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data['non_field_errors'][0], 'total price != check_price')
 
+    def test_menu_count(self):
+        pass
 
 class OrderListTestCase(APITestCase):
     """주문 내역 리스트"""
