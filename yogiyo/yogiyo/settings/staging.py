26be296288e8457dc686d35053e26bdbc590a00d
김주원 <kjw11077@naver.com>
2020-10-19 12:39:30 +0900

    config django-debug-toolbar, static

--- yogiyo/yogiyo/settings/staging.py
+++ yogiyo/yogiyo/settings/staging.py
@@ -2,3 +2,10 @@ from .base import *
 
 PROFILE = 'staging'
 DEBUG = True
+
+DEFAULT_FILE_STORAGE = 'core.asset_storage.MediaStorage'
+STATICFILES_STORAGE = 'core.asset_storage.StaticStorage'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
